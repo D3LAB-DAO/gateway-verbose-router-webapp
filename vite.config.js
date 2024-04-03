cae247f39f17e09d020c26e930dddc5efd4784aa
lukepark327 <lukepark327@gmail.com>
2024-04-04 00:14:05 +0900

    feat: fix route

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/verbose-router-webapp/'
+  base: '/gateway-verbose-router-webapp/'
 })
